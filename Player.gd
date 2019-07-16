f3bd8b939d242771da50ca2e84d4270806b60c63
Paul <tilionthewayward@gmail.com>
2019-07-16 22:21:10 +1000

    Touch controls.

--- Player.gd
+++ Player.gd
@@ -3,25 +3,47 @@ extends Area2D
 signal hit
 
 # Declare member variables here.
-export var speed = 400 # HOw fast the palyer will move (pixels/sec).
+export var speed = 400 # How fast the palyer will move (pixels/sec).
+var velocity = Vector2()
 var screen_size # Size of the game window
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	# Initial target is the start position.
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
+func _process(delta):
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2() # The palyer's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+#func _process(delta):
+#	var velocity = Vector2() # The palyer's movement vector.
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
 	if velocity .length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -29,8 +51,9 @@ func _process(delta):
 		$AnimatedSprite.stop()
 
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	# We don't need to clamp the player's postion with touch controls, becasue you can't click outside the screen
+	#position.x = clamp(position.x, 0, screen_size.x)
+	#position.y = clamp(position.y, 0, screen_size.y)
 
 	# Animation
 	if velocity.x != 0:
@@ -45,8 +68,3 @@ func _on_Player_body_entered(body):
 	hide() # Player disapperas after being hit
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
